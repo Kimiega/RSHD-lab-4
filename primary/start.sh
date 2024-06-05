9bf8bfffab1e7d9b5eb9d6a946732d9cebbb5750
vityaman <vityaman.dev@yandex.ru>
2024-06-05 16:04:56 +0300

    Did something

--- primary/start.sh
+++ primary/start.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/sh
 
 pg_rewind \
-    --config-file=/postgresql.conf \
+    --config-file=/data/postgres/postgresql.conf \
     --source-server='host=postgres_replica user=postgres password=password' \
     --target-pgdata=/data/postgres
 
-postgres -c 'config_file=/postgresql.conf'
+postgres -c 'config_file=/data/postgres/postgresql.conf'

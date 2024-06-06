01a46be4cd74334aecb80c2272a18e4a4ab89330
vityaman <vityaman.dev@yandex.ru>
2024-06-06 10:35:06 +0300

    Try to rewind postgres

--- primary/start.sh
+++ primary/start.sh
@@ -1,8 +1,39 @@
 #!/usr/bin/sh
 
-pg_rewind \
-    --config-file=/data/postgres/postgresql.conf \
-    --source-server='host=postgres_replica user=postgres password=password' \
-    --target-pgdata=/data/postgres
+echo "Checking for an available standby..."
+pg_isready \
+    --host="standby" \
+    --port="5432" \
+    --username="postgres" \
+    --dbname="postgres" \
+    --quiet
 
-postgres -c 'config_file=/data/postgres/postgresql.conf'
+if [ "$?" = "0" ]; then
+    echo "Found available standby. Starting rewind..."
+    su -p postgres -c "pg_rewind \
+        --config-file=/data/postgres/postgresql.conf \
+        --source-server='host=postgres_replica user=postgres password=password' \
+        --target-pgdata=/data/postgres"
+else
+    echo "Available standby was not found. Starting initialization..."
+
+    mkdir -p "$PGDATA" &&
+        chown -R postgres:postgres "$PGDATA" &&
+        chmod 700 "$PGDATA"
+
+    echo "password" >/password.txt &&
+        chown -R postgres:postgres password.txt &&
+        chmod 700 password.txt
+
+    su -p postgres -c "initdb \
+        --pgdata="$PGDATA" \
+        --auth-host=md5 \
+        --pwfile=/password.txt"
+
+    echo "host all all all md5" >>$PGDATA/pg_hba.conf
+    echo "host replication all all md5" >>$PGDATA/pg_hba.conf
+fi
+
+echo "Starting postgres..."
+su -p postgres -c "
+    postgres -c 'config_file=/config/postgresql.conf'"
